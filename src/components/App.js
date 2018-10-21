f8ea26645505bc7b9aca4b9c8afed53afb33a801
Advocad <advocad15@gmail.com>
2018-10-21 20:46:05 +0500

    Проверка

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>My RetApp!</h1>
+                <h1>My  RetApp!</h1>
             </div>
         );
     }
